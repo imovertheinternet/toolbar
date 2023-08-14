ffc157a008a11d61448a78fef89aced768f5c8a0
Conrad Grant <imovertheinternet@gmail.com>
2023-08-14 09:22:45 -0500

    WOY and uptime setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use chrono::{Datelike, Local};
-use std::sync::{mpsc, Mutex};
-use std::thread::sleep;
-use std::time::Duration;
-use std::{sync::Arc, thread};
-use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent, Manager};
-
+// use std::sync::{mpsc, Mutex};
+// use std::thread::sleep;
+// use std::time::Duration;
+// use std::{sync::Arc, thread};
+use tauri::{
+    CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
 use async_std::channel;
+use sysinfo::{System, SystemExt};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -26,13 +28,20 @@ fn get_week_of_year() -> String {
     return woy.to_string();
 }
 
+fn get_uptime() -> u64 {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    let mut current_uptime = sys.uptime();
+    current_uptime = current_uptime / 60;
+    current_uptime = current_uptime / 1440;
+
+    println!("current uptime! {}", current_uptime);
+    current_uptime
+ 
+}
+
 fn main() {
-    // let week_of_year = Local::now().iso_week().week();
-    // let mut woy_payload = "Week Of Year: ".to_string();
-
-    //TODO: will need to look into Arc and atomic integers. I think that allows me to have a constantly changing variable
-    // shared between threads.
-
     // // 1. Start with a string
     // let woy_payload = String::from("Week of year:");
 
@@ -41,69 +50,53 @@ fn main() {
     // let shared_woy_payload = Arc::new(Mutex::new(woy_payload));
 
     // // 3. clone the arc instance so we can pass it to the thread. Effectively having multiple threads
-    // // share ownership
+    // // share ownership. This clone references the same place in memory. THIS IS COPYING HEAP DATA, NOT JUST A REFERENCE.
     // let thread_shared_woy_payload = shared_woy_payload.clone();
 
     // // Create the thread and use move to MOVE ownership of the variables to inside the thread.
     // let thread_handle = thread::spawn(move || {
     //     let when = Duration::from_secs(5);
     //     println!("inside of a thread");
+    //     let mut counter = 0;
     //     loop {
-    //         println!("inside loop");
     //         println!("before sleep");
     //         sleep(when);
     //         println!("after sleep");
+    //         counter = counter + 1;
+    //         println!("counter = {:?}", counter);
     //          let mut updated_woy_updated = thread_shared_woy_payload.lock().unwrap();
     //          updated_woy_updated.push_str("Add this to the end");
     //     }
-
-    //    // Now grab mutex and update the value
-    //    let mut updated_woy_updated = thread_shared_woy_payload.lock().unwrap();
-    //          updated_woy_updated.push_str("Add this to the end");
     // });
 
-    // // wait for thread to be done. then print final value
+    /*
+     * wait for thread to be done. then print final value
+     * This is fine for a single sync call. What if i want multiple updates?
+     */
     // thread_handle.join().unwrap();
     // println!("ouside of thread");
     // println!("{}",shared_woy_payload.lock().unwrap());
 
-    // 1. Start with a string
-    let woy_payload = String::from("Week of year:");
+    // // CHANNELS: Could do this instead of arc/mutex
+    // let (tx, rx) = mpsc::channel();
 
-    //2. need to wrap it in an ARC to ensure safe concurrent access. Mutex will allow us exclusive access
-    // to this string between threads to make sure no one steps on toes.
-    let shared_woy_payload = Arc::new(Mutex::new(woy_payload));
+    // thread::spawn(move || {
+    //     let when = Duration::from_secs(5);
+    //     println!("inside of a thread");
+    //     let mut counter = 0;
+    //     loop {
+    //         println!("before sleep");
+    //         sleep(when);
+    //         println!("after sleep");
+    //         counter = counter + 1;
+    //         tx.send(counter).unwrap();
+    //         println!("counter = {:?}", counter);
+    //     }
+    // });
 
-    // 3. clone the arc instance so we can pass it to the thread. Effectively having multiple threads
-    // share ownership
-    let thread_shared_woy_payload = shared_woy_payload.clone();
-
-    let (tx, rx) = mpsc::channel();
-    // Create the thread and use move to MOVE ownership of the variables to inside the thread.
-    let thread_handle = thread::spawn(move || {
-        let when = Duration::from_secs(5);
-        println!("inside of a thread");
-        let mut counter = 0;
-
-        loop {
-            println!("inside loop");
-            println!("before sleep");
-            sleep(when);
-            println!("after sleep");
-            counter = counter + 1;
-            tx.send(counter).unwrap();
-            println!("counter = {:?}", counter);
-            //  let mut updated_woy_updated = thread_shared_woy_payload.lock().unwrap();
-            //  updated_woy_updated.push_str("Add this to the end");
-        }
-    });
-
-    // wait for thread to be done. then print final value
-    // thread_handle.join().unwrap();
-    // println!("ouside of thread");
-    // println!("{}",shared_woy_payload.lock().unwrap());
-
-    // This works but blocks the main thread.
+    /*
+     * This handles multiple updates but blocks the main thread.
+     */
     // for received_value in rx {
     //     println!("received: {}", received_value);
     //     let current_counter = received_value;
@@ -133,7 +126,8 @@ fn main() {
     //     }
     // }
 
-    println!("after loop -=-=-=-=");
+    // println!("after loop -=-=-=-=");
+    // println!("received value {:?}", received_value);
 
     // // Now you can use the received value outside the loop
     // if let Some(value) = received_value {
@@ -142,57 +136,13 @@ fn main() {
     //     println!("No value received.");
     // }
 
-    // let finalvalue = rx.recv().unwrap();
-    // println!("final value {}", finalvalue.to_string());
-    // thread::spawn(|| {
-    //     // every 5 seconds hit this function forever.
-    //     let wait_time = Duration::from_secs(5);
-    //     let woy_payload = "Week Of Year:".to_string();
-    //     loop {
-    //         // Get current time
-
-    //         // wait for 5 seconds
-    //            println!("Before sleep");
-    //         sleep(wait_time);
-    //         println!("after sleep");
-    //         // get week of year
-    //         let mut woy = get_week_of_year();
-    //         woy_payload.push_str(woy);
-    //         println!("payload! {}", woy_payload);
-
-    //         // // get the time
-    //         // let mut current_time = Local::now();
-    //         // let mut current_min_1 = Local::now().minute();
-    //         // get_current_min();
-    //         // println!("current time {:?}", current_time);
-    //         // println!("min 1 {:?}", current_min_1);
-    //         // // println!("min 2{:?}", current_min_2);
-    //         // // has the minute increased?
-
-    //         // // yes, call function to get new time
-
-    //         // // no, do nothing.
-    //         // println!("Before sleep");
-    //         // sleep(wait_time);
-    //         // println!("after sleep");
-    //         // // Check if the date has changed.
-    //     }
-    // });
-
-    // let current_second_of_year = Local::now().timestamp_millis();
-    // println!("second {}", current_second_of_year.to_string());
-
-    //"&" Borrows from the inital WOY varaible. After flow control chapter itll talk about ownership/borrowing.
-    // woy_payload.push_str(&week_of_year.to_string());
-
-    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
-    // let quit = CustomMenuItem::new("WOY".to_string(), woy_payload);
+    let uptime_menu_item = CustomMenuItem::new("uptime".to_string(), "n/a");
     let woy = CustomMenuItem::new("woy".to_string(), "n/a");
     let hide = CustomMenuItem::new("hide".to_string(), "Hide");
     let tray_menu = SystemTrayMenu::new()
-        // .add_item(quit)
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(woy)
+        .add_item(uptime_menu_item)
         .add_item(hide);
 
     let tray = SystemTray::new().with_menu(tray_menu);
@@ -201,10 +151,20 @@ fn main() {
         .system_tray(tray)
         .invoke_handler(tauri::generate_handler![greet, my_first_command])
         .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::LeftClick { tray_id, position, size, .. } => {
-                println!("waaaaaa");
-                let updatedWOY = get_week_of_year();
-                app.tray_handle().get_item("woy").set_title(updatedWOY);
+            SystemTrayEvent::LeftClick {
+                tray_id,
+                ..
+            } => {
+                println!("tray_id = {}", tray_id);
+                let week_of_year = get_week_of_year();
+                let mut week_of_year_payload = String::from("Week Of Year = ");
+                week_of_year_payload.push_str(&week_of_year);
+
+                let uptime = get_uptime();
+                let mut uptime_payload = String::from("Uptime =>");
+                uptime_payload.push_str(&uptime.to_string());
+                app.tray_handle().get_item("woy").set_title(week_of_year_payload).unwrap();
+                app.tray_handle().get_item("uptime").set_title(uptime_payload).unwrap();
             }
             _ => {}
         })
